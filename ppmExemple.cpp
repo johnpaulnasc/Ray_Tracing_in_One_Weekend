88ffb431eb44486ae00ee1dc6f343c154135ebab
João Paulo <73609454+jonhpaul5@users.noreply.github.com>
2021-06-09 01:10:15 -0300

    Update ppmExemple.cpp

--- ppmExemple.cpp
+++ ppmExemple.cpp
@@ -13,8 +13,8 @@ int main() {
 
     for (int j = image_height-1; j >= 0; --j) {
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
             auto b = 0.25;
 
             int ir = static_cast<int>(255.999 * r);
